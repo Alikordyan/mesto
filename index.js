78070b2fcddf31d2617406bb3f1209cd44fb60ca
Alberto Apresyan <apresyan42@mail.ru>
2021-03-01 20:57:41 +0300

    JS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close-button');
 
 const profile = document.querySelector('.profile');
-const editButton = profile.querySelector('.profile__edit-button'); // добавляем кнопки
+const editButton = profile.querySelector('.profile__edit-button');
 
 let profileName = profile.querySelector('.profile__name');
 let profileJob = profile.querySelector('.profile__subtitle');
@@ -12,26 +12,23 @@ const formElement = document.querySelector('.edit-form');
 let nameInput = formElement.querySelector('.edit-form__item_el_name');
 let jobInput = formElement.querySelector('.edit-form__item_el_job');
 
-// инициализация значения value и input
+
 function setInputValue() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-  console.log(nameInput.value);
 }
 
 function openPopup() {
   popup.classList.add('popup_opened');
-  setInputValue();//открытие
+  setInputValue();
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened'); // закрытие
+  popup.classList.remove('popup_opened'); 
 }
 
-editButton.addEventListener('click', openPopup); //клик открытие 
-closeButton.addEventListener('click', closePopup); // клик закрытие 
-
-
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
 
 function formSubmitHandler (evt) {
@@ -39,7 +36,7 @@ function formSubmitHandler (evt) {
 
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopup(); // сохраняем данные при внесения изменения
+  closePopup();
 
 }
 
